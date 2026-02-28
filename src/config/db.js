0636d01023f31da92897835e7f75a885e16adbb1
RH Mohamed Amar <rabiloumohamed2530@gmail.com>
2026-02-28 16:02:16 +0100

    fix db.js et gitignore

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,5 @@
 const { Pool } = require("pg");
 
-/*
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-});
-*/
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
